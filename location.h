a96764357086b60c9bdfdbffff487467b3e4036e
LaptopBean <evany227@gmail.com>
2025-11-15 14:16:34 -0800

    checking fstream

---
+++ location.h
@@ -0,0 +1,16 @@
+#include <string>
+
+class Location {
+private:
+    double latitude;
+    double longitude;
+    int speedLimitMPH = -1;
+
+public:
+    Location(double lat, double lon);
+    Location();
+    bool fetchSpeedLimit();
+    double getLatitude() const;
+    double getLongitude() const;
+    int getSpeedLimit() const;
+};
