89d633a2c2e19e1232567171045aea22c9eb9f6e
myctruongg <mychautruong97@gmail.com>
2025-11-15 18:35:51 -0800

    adding all the gd files, data not yet connected and fixed intro.cd

--- intro.gd
+++ intro.gd
@@ -1,6 +1,38 @@
 extends Control
 
-func _on_StartButton_pressed() -> void:
-    # Load your Upload Page
-    var upload_scene = load("res://UploadPage.tscn").instantiate()
-    get_tree().change_scene_to_packed(upload_scene)
+func _ready() -> void:
+    pass
+
+
+# When the upload button is clicked
+func _on_UploadButton_pressed() -> void:
+    $FileDialog.popup()   # open the file explorer
+
+
+# When the user selects a file in the FileDialog
+func _on_FileDialog_file_selected(path: String) -> void:
+    print("Selected file:", path)
+    process_csv(path)     # send the file to backend
+
+
+# Run your C++ program from Godot
+func process_csv(path: String) -> void:
+    var output := []
+    
+    # IMPORTANT:
+    # "scorecalc" must be your compiled C++ binary placed in your Godot project folder
+    var exit_code = OS.execute("res://scorecalc", [path], output)
+
+    if exit_code != 0:
+        print("C++ backend error:", exit_code)
+        return
+
+    var json_string := output.join("\n")
+    go_to_summary(json_string)
+
+
+# Switch to the drive summary screen and send data
+func go_to_summary(json_string: String) -> void:
+    var scene := load("res://drive_summary.tscn").instantiate()
+    scene.load_results(json_string)
+    get_tree().change_scene_to_packed(scene)
